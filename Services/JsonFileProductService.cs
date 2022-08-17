c7ea84b9fdfdc5dd95381cf4fb9762350e08f1a0
Nika Nabakhteveli <nabakhtevelin@gmail.com>
2022-08-17 22:56:41 +0400

    Added JsonFileProduct service

---
+++ Services/JsonFileProductService.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using ContosoCrafts.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ContosoCrafts.Services;
+
+public class JsonFileProductService
+{
+    public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+    {
+        WebHostEnvironment = webHostEnvironment;
+    }
+
+    public IWebHostEnvironment WebHostEnvironment { get; }
+
+    private string JsonFileName
+    {
+        get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+    }
+
+    public IEnumerable<Product> GetProducts()
+    {
+        using(var jsonFileReader = File.OpenText(JsonFileName))
+        {
+            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+        }
+    }
+}
