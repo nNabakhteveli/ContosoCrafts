5285c1438cb1adcc6cdd7ae8019c539e6778dc8d
Nika Nabakhteveli <nabakhtevelin@gmail.com>
2022-08-17 21:29:53 +0400

    Initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ContosoCrafts.Models;
+
+public class Product
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+    
+    [JsonPropertyName("maker")]
+    public string Maker { get; set; }
+    
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+    
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+    
+    public int[] Ratings { get; set; }
+
+    public override string ToString() => JsonSerializer.Serialize<Product>(this);
+}
